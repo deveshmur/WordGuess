45b84b89993561f956b875a4da7dfc8f5586e691
deveshmur <dmdevesh@gmail.com>
2025-11-03 12:00:19 -0500

    Set up game with instance variables, constructors, strings, and starting message.

---
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -0,0 +1,21 @@
+package com.github.zipcodewilmington;
+
+
+/**
+ * @author xt0fer
+ * @version 1.0.0
+ * @date 5/27/21 11:02 AM
+ */ 
+public class WordGuess {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to Word Guess! Try to guess the word I'm thinking of. You have 3 attempts. Good luck!😈");
+    }
+
+    
+
+
+
+
+
+}
--- java/src/main/java/com/github/zipcodewilmington/sample/Person.java
+++ java/src/main/java/com/github/zipcodewilmington/sample/Person.java
@@ -1,4 +1,47 @@
 package com.github.zipcodewilmington.sample;
 
 public class Person {
+
+    
+    private String firstName;
+    private String lastName;
+    private Integer getAge;
+
+
+    public Person() {}
+    public Person(String firstName, String lastName, Integer age) {
+        this.firstName = "";
+        this.lastName = "";
+        this.getAge = age;
 }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public Integer getAge() {
+        return getAge;
+    }
+
+    public void setAge(Integer age) {
+        this.getAge = age;
+    }
+
+
+
+}
+
